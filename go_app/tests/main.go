5eaab80414573cd5080b3f1f8e1e35842aa9fe6b
John Harmon <placeholder>
2025-07-27 18:45:27 -0600

    adding in json schema dump for types

--- go_app/tests/main.go
+++ go_app/tests/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"net/url"
 	"os"
@@ -10,6 +12,8 @@ import (
 var (
 	testCigarInsertion bool
 	apiURL             string
+	dumpJson           bool
+	output             string
 	lInfo              = log.New(os.Stdout, "INFO: ", log.LstdFlags)
 	lErr               = log.New(os.Stderr, "ERROR: ", log.LstdFlags)
 	testCigarQuery     bool
@@ -26,13 +30,35 @@ func main() {
 		}
 		CreateAndSendCigarCreationPayload(testURL)
 	}
+	if dumpJson {
+		outputWriter := ParseOutputValue()
+		DumpToJson(&Cigar{}, outputWriter)
+	}
 }
 
 func RegisterAndParseCLIArgs() {
-	flag.BoolVar(&testCigarInsertion, "test-cigar-insertion", true, "Boolean toggle for testing inserting cigars into the DB via the api")
+	flag.BoolVar(&testCigarInsertion, "test-cigar-insertion", false, "Boolean toggle for testing inserting cigars into the DB via the api")
 	flag.BoolVar(&testCigarQuery, "query", false, "Boolean toggle for testing a SELECT * from the cigar table")
+	flag.BoolVar(&dumpJson, "dump", false, "Boolean toggle to dump json schema for objects")
 	flag.StringVar(&apiURL, "api-url", "http://localhost:8080/api/test", "Url to use for testing the api")
 	flag.StringVar(&apiURL, "host", "http://localhost:8080/api/test", "Url to use for testing the api")
+	flag.StringVar(&output, "output", "stdout", "Location for any output, defaults to stdout")
 	// flag.IntVar(&port, "port", 8080, "Port to use for accessing the api")
 	flag.Parse()
+}
+
+func ParseOutputValue() io.Writer {
+	switch output {
+	case "stdout":
+		return os.Stdout
+	case "stderr":
+		return os.Stderr
+	default:
+		f, err := os.OpenFile(output, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o755)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error opening output file (%s): %s", output, err)
+			return os.Stdout
+		}
+		return f
+	}
 }
