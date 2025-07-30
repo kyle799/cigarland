064e35fb7ebf454e2f5dec07e8ab72623f4c1cc0
John Harmon <placeholder>
2025-07-29 20:49:25 -0600

    finished query filter generation functions. working on adapting existing handlers via an enclosing wrapper function.

--- go_app/routes.go
+++ go_app/routes.go
@@ -10,7 +10,7 @@ import (
 func SetRoutesAndRun(router *gin.Engine) {
 	router.GET(fmt.Sprintf("%s/test", apiPrefix), test)
 	router.POST(fmt.Sprintf("%s/test", apiPrefix), HandleCreateCigarRouter)
-	router.GET(fmt.Sprintf("%s/cigars", apiPrefix), HandleQueryCigarTable)
+	router.GET(fmt.Sprintf("%s/cigars", apiPrefix), MakeDBHandler(cigarDB, HandleQueryCigarTable))
 	log.Printf("starting webserver at %s:%d", server, port)
 	router.Run(fmt.Sprintf("%s:%d", server, port))
 }
